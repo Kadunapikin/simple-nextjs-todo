6309889219e63803a491d4260f9622005edcd6d3
kadunapikin <ibakoshi27@gmail.com>
2023-11-23 15:38:45 +0100

    add header to page.tsx in new dir

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,5 +1,10 @@
 export default function () {
     return (
-        <h1>new</h1>
+        <>
+            <header className="flex justify-between items-center mb-4">
+                <h1 className="text-2xl">New</h1>
+            </header>
+
+        </>
     )
 }
