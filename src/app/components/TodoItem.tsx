ea97d07372ad831b0ac86c7ef23de086e6e806cb
kadunapikin <ibakoshi27@gmail.com>
2023-11-23 15:17:52 +0100

    add input and label tag to TodoItem and style them

--- src/app/components/TodoItem.tsx
+++ src/app/components/TodoItem.tsx
@@ -6,8 +6,9 @@ type TodoItemProps = {
 
 export function TodoItem({ id, title, complete }: TodoItemProps) {
     return (
-        <li>
-            
+        <li className="flex gap-1 items-center">
+            <input id={id} type="checkbox" className="cursor-pointer peer" />
+            <label htmlFor={id} className="peer-checked:line-through">{title}</label>
         </li>
     )
 }
