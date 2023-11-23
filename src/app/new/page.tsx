27183a144d04564ff5f3a9ada3c1adc6b61ef7cc
kadunapikin <ibakoshi27@gmail.com>
2023-11-23 16:04:30 +0100

    style cancel link and Create button

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -9,9 +9,17 @@ export default function () {
             <form className="flex gap-2 flex-col">
                 <input type="text" name="title" className="border border-slate-300 bg-transparent rounded px-2 py-1 outline-none focus-within:border-slate-100"/>
             </form>
-            <div>
-                <Link href='..'>Cancel</Link>
-                <button type="submit"></button>
+            <div className="flex gap-1 pt-3 justify-end">
+                <Link 
+                    href='..' 
+                    className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none">
+                    Cancel
+                </Link>
+                <button 
+                    type="submit" 
+                    className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none">
+                    Create
+                </button>
             </div>
 
         </>
